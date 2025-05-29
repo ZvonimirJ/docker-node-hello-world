398e11b73cd1721210257e71de935c074df3df59
ZvonimirJ <zjukic22@unizd.hr>
2025-05-29 23:02:41 +0000

    add workflow

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var http = require('http')
+var os = require('os')
 
 var port = 8080
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
+  response.end('Hello from ' + os.hostname() + '\n')
 })
 
 server.listen(port)
